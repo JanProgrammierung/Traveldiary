541abd340a76ff5e321311d97acfb8417d78e5f5
JanProgrammierung <JanProgrammierung@users.noreply.github.com>
2019-02-18 14:41:42 +0100

    Beginn

---
+++ Traveldiary/src/main/java/de/hsba/bi/traveldiary/Traveldiary/TraveldiaryApplication.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.traveldiary.Traveldiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveldiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveldiaryApplication.class, args);
+	}
+
+}
---
+++ Traveldiary/src/test/java/de/hsba/bi/traveldiary/Traveldiary/TraveldiaryApplicationTests.java
@@ -0,0 +1,16 @@
+package de.hsba.bi.traveldiary.Traveldiary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TraveldiaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
